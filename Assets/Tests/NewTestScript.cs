29db364b027b9c77d8fd1f0cd8241d4c64baec3f
dovantra <dovantranhahuong@gmail.com>
2025-04-18 19:21:39 +0700

    test jump

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -26,6 +26,30 @@ public class NewTestScript
         
         yield return new WaitForSeconds(1f);
      Assert.Greater(go.transform.position.x, startX);
-    
+
+    }
+
+
+    [UnityTest] 
+    public IEnumerator NewTestScriptPlayerJump()
+    {
+        GameObject player = new GameObject();
+        Rigidbody rb = player.AddComponent<Rigidbody>();
+        rb.useGravity = true;
+
+        float jumpForce = 4f;
+
+        // Lưu lại vị trí Y ban đầu
+        float startY = player.transform.position.y;
+
+        // Áp lực nhảy
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+        // Đợi vài frame để vật lý xử lý (khoảng 0.5s)
+        yield return new WaitForSeconds(0.5f);
+
+        // Kiểm tra vị trí mới cao hơn vị trí ban đầu → nghĩa là đã nhảy
+        Assert.Greater(player.transform.position.y, startY);
+
     }
 }
